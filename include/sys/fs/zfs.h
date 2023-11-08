93034e11511c42ae088e34262149ac850e2dff15
Don Brady <don.brady@delphix.com>
2023-11-08 11:19:41 -0700

    RAID-Z expansion feature
    This feature allows disks to be added one at a time to a RAID-Z group,
    expanding its capacity incrementally.  This feature is especially useful
    for small pools (typically with only one RAID-Z group), where there
    isn't sufficient hardware to add capacity by adding a whole new RAID-Z
    group (typically doubling the number of disks).
    
    == Initiating expansion ==
    
    A new device (disk) can be attached to an existing RAIDZ vdev, by
    running `zpool attach POOL raidzP-N NEW_DEVICE`, e.g. `zpool attach tank
    raidz2-0 sda`.  The new device will become part of the RAIDZ group.  A
    "raidz expansion" will be initiated, and the new device will contribute
    additional space to the RAIDZ group once the expansion completes.
    
    The `feature@raidz_expansion` on-disk feature flag must be `enabled` to
    initiate an expansion, and it remains `active` for the life of the pool.
    In other words, pools with expanded RAIDZ vdevs can not be imported by
    older releases of the ZFS software.
    
    == During expansion ==
    
    The expansion entails reading all allocated space from existing disks in
    the RAIDZ group, and rewriting it to the new disks in the RAIDZ group
    (including the newly added device).
    
    The expansion progress can be monitored with `zpool status`.
    
    Data redundancy is maintained during (and after) the expansion.  If a
    disk fails while the expansion is in progress, the expansion pauses
    until the health of the RAIDZ vdev is restored (e.g. by replacing the
    failed disk and waiting for reconstruction to complete).
    
    The pool remains accessible during expansion.  Following a reboot or
    export/import, the expansion resumes where it left off.
    
    == After expansion ==
    
    When the expansion completes, the additional space is available for use,
    and is reflected in the `available` zfs property (as seen in `zfs list`,
    `df`, etc).
    
    Expansion does not change the number of failures that can be tolerated
    without data loss (e.g. a RAIDZ2 is still a RAIDZ2 even after
    expansion).
    
    A RAIDZ vdev can be expanded multiple times.
    
    After the expansion completes, old blocks remain with their old
    data-to-parity ratio (e.g. 5-wide RAIDZ2, has 3 data to 2 parity), but
    distributed among the larger set of disks.  New blocks will be written
    with the new data-to-parity ratio (e.g. a 5-wide RAIDZ2 which has been
    expanded once to 6-wide, has 4 data to 2 parity).  However, the RAIDZ
    vdev's "assumed parity ratio" does not change, so slightly less space
    than is expected may be reported for newly-written blocks, according to
    `zfs list`, `df`, `ls -s`, and similar tools.
    
    Sponsored-by: The FreeBSD Foundation
    Sponsored-by: iXsystems, Inc.
    Sponsored-by: vStack
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Reviewed-by: Mark Maybee <mark.maybee@delphix.com>
    Authored-by: Matthew Ahrens <mahrens@delphix.com>
    Contributions-by: Fedor Uporov <fuporov.vstack@gmail.com>
    Contributions-by: Stuart Maybee <stuart.maybee@comcast.net>
    Contributions-by: Thorsten Behrens <tbehrens@outlook.com>
    Contributions-by: Fmstrat <nospam@nowsci.com>
    Contributions-by: Don Brady <dev.fs.zfs@gmail.com>
    Signed-off-by: Don Brady <dev.fs.zfs@gmail.com>
    Closes #15022
    
    Signed-off-by: George Amanakis <gamanakis@gmail.com>

--- include/sys/fs/zfs.h
+++ include/sys/fs/zfs.h
@@ -726,6 +726,7 @@ typedef struct zpool_load_policy {
 #define	ZPOOL_CONFIG_SCAN_STATS		"scan_stats"	/* not stored on disk */
 #define	ZPOOL_CONFIG_REMOVAL_STATS	"removal_stats"	/* not stored on disk */
 #define	ZPOOL_CONFIG_CHECKPOINT_STATS	"checkpoint_stats" /* not on disk */
+#define	ZPOOL_CONFIG_RAIDZ_EXPAND_STATS	"raidz_expand_stats" /* not on disk */
 #define	ZPOOL_CONFIG_VDEV_STATS		"vdev_stats"	/* not stored on disk */
 #define	ZPOOL_CONFIG_INDIRECT_SIZE	"indirect_size"	/* not stored on disk */
 
@@ -791,6 +792,8 @@ typedef struct zpool_load_policy {
 #define	ZPOOL_CONFIG_SPARES		"spares"
 #define	ZPOOL_CONFIG_IS_SPARE		"is_spare"
 #define	ZPOOL_CONFIG_NPARITY		"nparity"
+#define	ZPOOL_CONFIG_RAIDZ_EXPANDING	"raidz_expanding"
+#define	ZPOOL_CONFIG_RAIDZ_EXPAND_TXGS	"raidz_expand_txgs"
 #define	ZPOOL_CONFIG_HOSTID		"hostid"
 #define	ZPOOL_CONFIG_HOSTNAME		"hostname"
 #define	ZPOOL_CONFIG_LOADED_TIME	"initial_load_time"
@@ -908,6 +911,15 @@ typedef struct zpool_load_policy {
 
 #define	VDEV_TOP_ZAP_ALLOCATION_BIAS \
 	"org.zfsonlinux:allocation_bias"
+
+#define	VDEV_TOP_ZAP_RAIDZ_EXPAND_STATE \
+	"org.openzfs:raidz_expand_state"
+#define	VDEV_TOP_ZAP_RAIDZ_EXPAND_START_TIME \
+	"org.openzfs:raidz_expand_start_time"
+#define	VDEV_TOP_ZAP_RAIDZ_EXPAND_END_TIME \
+	"org.openzfs:raidz_expand_end_time"
+#define	VDEV_TOP_ZAP_RAIDZ_EXPAND_BYTES_COPIED \
+	"org.openzfs:raidz_expand_bytes_copied"
 
 /* vdev metaslab allocation bias */
 #define	VDEV_ALLOC_BIAS_LOG		"log"
@@ -1139,6 +1151,16 @@ typedef struct pool_removal_stat {
 	 */
 	uint64_t prs_mapping_memory;
 } pool_removal_stat_t;
+
+typedef struct pool_raidz_expand_stat {
+	uint64_t pres_state; /* dsl_scan_state_t */
+	uint64_t pres_expanding_vdev;
+	uint64_t pres_start_time;
+	uint64_t pres_end_time;
+	uint64_t pres_to_reflow; /* bytes that need to be moved */
+	uint64_t pres_reflowed; /* bytes moved so far */
+	uint64_t pres_waiting_for_resilver;
+} pool_raidz_expand_stat_t;
 
 typedef enum dsl_scan_state {
 	DSS_NONE,
@@ -1605,6 +1627,7 @@ typedef enum {
 	ZPOOL_WAIT_RESILVER,
 	ZPOOL_WAIT_SCRUB,
 	ZPOOL_WAIT_TRIM,
+	ZPOOL_WAIT_RAIDZ_EXPAND,
 	ZPOOL_WAIT_NUM_ACTIVITIES
 } zpool_wait_activity_t;
 
